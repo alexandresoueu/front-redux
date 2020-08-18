7ded21c95a97519143beb4f9afd8ff33deb78554
calexandredev <calexandredev@gmail.com>
2020-08-18 00:28:48 -0300

    Create Cards and CSS

--- redux-basic/src/App.js
+++ redux-basic/src/App.js
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <h1>React Redux exercise</h1>
       <div className='line'>
-      <Cards />
-      <Cards />
-      <Cards />
+      <Cards title='Card ONE' red>REDzin</Cards>
+      <Cards title='Card TWO' blue>BLUEzin</Cards>
+      </div>
+      <div className='line'>
+      <Cards  title='Card THREE' green>GREENzin</Cards>
+      <Cards title='Card FOUR' purple>PURPLEzin</Cards>
       </div>
     </div>
   );
--- redux-basic/src/components/Cards.js
+++ redux-basic/src/components/Cards.js
@@ -2,9 +2,24 @@ import './Cards.css'
 
 import React from 'react'
 
+const getColor = (props) => {
+  if(props.red) return 'Red'
+  if(props.green) return 'Green'
+  if(props.blue) return 'Blue'
+  if(props.purple) return 'Purple'
+  return ''
+}
+
 const Cards = props => {
   return (
-    <div>CARDS</div>
+    <div className={`Card ${getColor(props)}`}>
+      <div className='Header'>
+        <span className='Title'>{props.title}</span>
+      </div>
+      <div className='Content'>
+        {props.children}
+      </div>
+    </div>
   )
 }
 
