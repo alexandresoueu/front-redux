dcc9587263d40772d7d55e18893d3ccd4f1458d5
calexandredev <calexandredev@gmail.com>
2020-08-21 18:45:11 -0300

    Refactored reducer

---
+++ redux-basic/src/store/reducers/numbersReducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+    min: 1,
+    max: 10,
+
+}
+
+const numbersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PUT_MIN_VALUE':
+      return {
+        ...state,
+        min: action.payload
+      }
+    case 'PUT_MAX_VALUE':
+      return {
+        ...state,
+        max: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+export default numbersReducer
--- redux-basic/src/store/storeConfig.js
+++ redux-basic/src/store/storeConfig.js
@@ -1,25 +1,9 @@
 import { createStore, combineReducers } from 'redux'
+import numbersReducer from './reducers/numbersReducer'
+
 
 const reducers = combineReducers({
-  numbers: function (state, action) {
-    switch (action.type) {
-      case 'PUT_MIN_VALUE':
-        return {
-          ...state,
-          min: action.payload
-        }
-      case 'PUT_MAX_VALUE':
-        return {
-          ...state,
-          max: action.payload
-        }
-      default:
-        return {
-          min: 1,
-          max: 99,
-        }
-    }
-  }
+  numbers: numbersReducer
 })
 
 function storeConfig() {
